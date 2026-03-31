2f3489247972d9b099a62d4483d02c11ef24b37d
Luca Datti <datti.luca@gmail.com>
2026-03-31 18:55:43 +0200

    Added ResNet18 implementation

--- models/resnet.py
+++ models/resnet.py
@@ -40,69 +40,81 @@ That is the secret: By simply adding the input back at the end, you ensure the n
 than a shallow network, because it can always just choose to "do nothing" and pass $x$ through.
 
 '''
+class BasicBlock(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, stride=1):
+        
+        super(BasicBlock,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
 
-class ResNet(nn.Module):
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride!=1 or in_channels!=out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels,out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+        def forward(self,x):
+
+            out = self.conv1(x) 
+            out = self.bn1(out)
+            out = self.relu(out)
+            out = self.conv2(out)
+            out = self.bn2(out)
+            out += self.shortcut(x)
+            out = self.relu(out)
+
+            return out
+
+class ResNet18(nn.Module):
     def __init__(self, num_classes=200):
 
-        super(ResNet,self).__init__()
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=2) 
+        super(ResNet18,self).__init__()
+ 
+        self.in_channels = 64
         
-        # going from 224x244 to 112x112
+        self.conv1 = nn.Conv2d(3,64, kernel_size=3, stride=1, padding=1, bias=False) 
+        self.bn1 = nn.BatchNorm2d(64)
+        self.ReLU = nn.ReLU(inplace=True)
 
-        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.layer1 = self._make_layer(BasicBlock,64,2,stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256,2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock,512,2,stride=2)
 
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
 
-    def forward(self,x):
+        self.fc = nn.Linear(512,num_classes)
 
-        # convolutional layers
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
 
-        x=self.conv1(x)
+        for stride in strides:
+            layers.append(block(self.in_channels,out_channels,stride))
+            self.in_channels = out_channels
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
         
-        x=F.relu(x)
-
-        x=self.maxpool1(x)
-
-        x=self.conv2(x)
-
-        x=F.relu(x)
-
-        x=self.maxpool2(x)
-
-        x=self.conv3(x)
-
-        x=F.relu(x)
-
-        x=self.conv4(x)
-
-        x=F.relu(x)
-
-        x=self.conv5(x)
-
-        x=F.relu(x)
-
-        x=self.maxpool3(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
         
-
-        x=torch.flatten(x,1)
-
-        # fully connected layers
-
-        x=self.fc1(x)
-
-        x=F.relu(x)
-
-        x=self.do1(x)
-
-        x=self.fc2(x)
-        
-        x=F.relu(x)
-
-        x=self.do2(x)
-
-        x=self.fc3(x)
-
-        return x
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
         
 
