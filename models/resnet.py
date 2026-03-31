8c9bb01960e86b1c1431c01a4a8ed1ebfe7c0332
Luca Datti <datti.luca@gmail.com>
2026-03-31 17:55:59 +0200

    added brief resnet explanation

--- models/resnet.py
+++ models/resnet.py
@@ -2,6 +2,45 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+'''
+
+CONTEXT: 
+
+Every consecutive winning architecture uses 
+more layers in a deep neural network 
+to lower the error rate after the first CNN-based
+architecture (AlexNet) that won the ImageNet 2012 competition.
+
+But when we add more layers, a typical deep learning issue known as 
+the Vanishing/Exploding gradient arises. 
+
+We apply a method known as skip connections in this network.
+
+# 1. Save the input (the 'shortcut')
+identity = x 
+
+# 2. Run the input through the layers (the 'conv block')
+out = convolution_layer_1(x)
+out = batch_norm(out)
+out = relu(out)
+out = convolution_layer_2(out)
+out = batch_norm(out)
+
+# 3. JUST ADD THEM TOGETHER
+# This is the "Residual" part!
+out = out + identity 
+
+# 4. Final touch
+out = relu(out)
+Why does this work?If those convolution layers in Step 2 are "bad" or haven't learned anything yet, 
+they will likely output something close to 0 (because weights are usually initialized to values very close to 0)
+In a normal network: $Output = 0$ (The network "dies" or loses the data).
+In a ResNet: $Output = 0 + x$. (The data survives! The network just passes the original image to the next layer).
+That is the secret: By simply adding the input back at the end, you ensure the network can never perform worse 
+than a shallow network, because it can always just choose to "do nothing" and pass $x$ through.
+
+'''
+
 class ResNet(nn.Module):
     def __init__(self, num_classes=200):
 
@@ -12,28 +51,6 @@ class ResNet(nn.Module):
         # going from 224x244 to 112x112
 
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-
-        self.conv2 = nn.Conv2d(96,256, kernel_size=5, padding=2)
-
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
-
-        self.conv3 = nn.Conv2d(256,384, kernel_size=3, padding=1)
-
-        self.conv4 = nn.Conv2d(384,384, kernel_size=3, padding=1)
-
-        self.conv5 = nn.Conv2d(384,256, kernel_size=3,padding=1)
-
-        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
-
-        self.fc1 = nn.Linear(9216,4096)
-
-        self.do1 = nn.Dropout(p=0.5) # to prevent overfitting
-        
-        self.fc2 = nn.Linear(4096,4096)
-
-        self.do2 = nn.Dropout(p=0.5) # to prevent overfitting
-
-        self.fc3 = nn.Linear(4096,num_classes)
 
 
     def forward(self,x):
