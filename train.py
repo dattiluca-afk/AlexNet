827abf60c2c87ee4cb500194897d4efc3a9c282d
Luca Datti <datti.luca@gmail.com>
2026-04-01 18:17:21 +0200

    switched to resnet2 for testing

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 
 # Local modules' import
 
-from models.resnet import ResNet18
+from models.resnet2 import ResNet18
 from dataset.dataset import prepare_data, get_dataloaders
 
 def train_one_epoch(epoch, model, train_loader, criterion, optimizer, device):
