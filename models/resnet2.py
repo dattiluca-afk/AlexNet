7d61b00ec6a52bf856a253f363219675696fc679
Luca Datti <datti.luca@gmail.com>
2026-04-02 16:03:29 +0200

    commit

--- models/resnet2.py
+++ models/resnet2.py
@@ -1,141 +1,126 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 class ResNet18(nn.Module):
 
     def __init__(self, num_classes=200):
-        
         super().__init__()
-
-        '''
-        That'd be good if images were 224x225. TinyImagnet's images are 64x64        
-
-        self.conv1 = nn.Conv2d(in_channels=3,out_channels=64,kernel_size=7,stride=2, padding=3) # CH: 3 -> 64; W: 224 -> 112
-        
-        self.bn1 = nn.BatchNorm2d(64)
-
-        self.relu = nn.ReLU(inplace=True)
-
-        self.mp = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)# CH: 64; W: 112 -> 56 
-        '''
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
 
-        # REMOVE maxpool completely
-
-        self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64,kernel_size=3,stride=1,padding=1),
+        # -------- STAGE 1 (64) --------
+        self.block1_1 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3,stride=1,padding=1), 
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
             nn.BatchNorm2d(64)
         )
 
-        # Shortcut for Block 2 to fix 64->128 and W 56->28
+        self.block1_2 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(64)
+        )
+
+        # -------- STAGE 2 (128) --------
         self.s2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(64, 128, 1, 2, bias=False),
             nn.BatchNorm2d(128)
         )
 
-        self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128,kernel_size=3,stride=2,padding=1), 
+        self.block2_1 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, 2, 1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128,kernel_size=3,stride=1,padding=1), 
+            nn.Conv2d(128, 128, 3, 1, 1, bias=False),
             nn.BatchNorm2d(128)
         )
-        
+
+        self.block2_2 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(128)
+        )
+
+        # -------- STAGE 3 (256) --------
         self.s3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(128, 256, 1, 2, bias=False),
             nn.BatchNorm2d(256)
         )
 
-        self.block3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256,kernel_size=3,stride=2 ,padding=1), 
+        self.block3_1 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, 2, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(256, 256, 3, 1, 1, bias=False),
             nn.BatchNorm2d(256)
         )
 
+        self.block3_2 = nn.Sequential(
+            nn.Conv2d(256, 256, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(256)
+        )
+
+        # -------- STAGE 4 (512) --------
         self.s4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(256, 512, 1, 2, bias=False),
             nn.BatchNorm2d(512)
         )
 
-        self.block4 = nn.Sequential(
-            nn.Conv2d(in_channels= 256, out_channels=512,kernel_size=3,stride=2,padding=1),
+        self.block4_1 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, 2, 1, bias=False),
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512,kernel_size=3,stride=1,padding=1), 
+            nn.Conv2d(512, 512, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(512)
+        )
+
+        self.block4_2 = nn.Sequential(
+            nn.Conv2d(512, 512, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, 1, 1, bias=False),
             nn.BatchNorm2d(512)
         )
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        self.fc = nn.Linear(512,num_classes)
+        self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
-      
-        '''
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.mp(out)
-        '''
-        
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        # NO maxpool
-        
-        # first block 
 
-        mem = out 
+        out = self.relu(self.bn1(self.conv1(x)))
 
-        out = self.block1(out)
+        # ---- STAGE 1 ----
+        out = self.relu(self.block1_1(out) + out)
+        out = self.relu(self.block1_2(out) + out)
 
-        out = self.relu(out+mem)
+        # ---- STAGE 2 ----
+        identity = self.s2(out)
+        out = self.relu(self.block2_1(out) + identity)
+        out = self.relu(self.block2_2(out) + out)
 
-        # second block
+        # ---- STAGE 3 ----
+        identity = self.s3(out)
+        out = self.relu(self.block3_1(out) + identity)
+        out = self.relu(self.block3_2(out) + out)
 
-        mem = self.s2(out)
-
-        out = self.block2(out)    
-
-        out = self.relu(out+mem)
-
-        # third block 
-
-        mem = self.s3(out)
-        
-        out = self.block3(out)
-
-        out = self.relu(out+mem)
-
-        # fourth block
-
-        mem = self.s4(out)
-
-        out = self.block4(out)
-        
-        out = self.relu(out+mem)
-
-        # final touch
+        # ---- STAGE 4 ----
+        identity = self.s4(out)
+        out = self.relu(self.block4_1(out) + identity)
+        out = self.relu(self.block4_2(out) + out)
 
         out = self.avgpool(out)
-
-        out = torch.flatten(out,1) #flatten before fc
-        
+        out = torch.flatten(out, 1)
         out = self.fc(out)
 
         return out
-
-
-
-
-
-
